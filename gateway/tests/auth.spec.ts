22fb12857d08ef32e772dc438ba1a95c94e0afc3
Andrei Moskalev <andrei.moskalev@alasco.de>
2022-07-11 19:42:40 +0200

    add tests support

---
+++ gateway/tests/auth.spec.ts
@@ -0,0 +1,11 @@
+// import supertest from "supertest";
+
+// const app = express();
+
+test.todo("User can succesfully login");
+test.todo("User receives 401 on expired token");
+test.todo("User can get new access token using refresh token");
+test.todo("User get 404 on invalid refresh token");
+test.todo("User can use refresh token only once");
+test.todo("Refresh tokens become invalid on logout");
+test.todo("Multiple refresh tokens are valid");
